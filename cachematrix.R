f61fd2b37d0fb7462b4aefd15f01d1ec4f69ea56
Philippo <Marie-Claire.Philippo@sanofi.com>
2023-06-01 14:48:43 +0200

    1rst commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+#setwd('C:/Users/fr14057/OneDrive - Sanofi/Coursera_BP/cours3')
+
+##set x as a matrix
+##set i as null
+##change mean to inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
-}
+  i <- NULL
+  set <- function(y){
+    x <<- y
+    i <<- NULL
+  }
+  get <- function()x
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function() i 
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
+  }
 
 
-## Write a short comment describing this function
+## Change mean to inverse
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  if(!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  mat <- x$get()
+  i <- solve(mat,...)
+  x$setInverse(i)
 }
+
+
+
